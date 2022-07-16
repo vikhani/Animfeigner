173c54b05f95ebd6ce87c3af16687ee633957404
vikhani <vikhanieva@gmail.com>
2022-07-16 16:57:37 +0300

    Credentials setting moved to env variables

--- src/main/java/com/vikhani/animfeigner/configs/ClientConfiguration.java
+++ src/main/java/com/vikhani/animfeigner/configs/ClientConfiguration.java
@@ -2,15 +2,23 @@ package com.vikhani.animfeigner.configs;
 
 import feign.auth.BasicAuthRequestInterceptor;
 
+import lombok.Setter;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@Setter
 public class ClientConfiguration {
+    @Value("${animventory.username:user}")
+    private String username;
+
+    @Value("${animventory.password:password}")
+    private String password;
 
     @Bean
-    public BasicAuthRequestInterceptor basicAuthRequestInterceptor(){
-        // TODO move credentials to environment variables
-        return new BasicAuthRequestInterceptor("user", "password");
+    public BasicAuthRequestInterceptor basicAuthRequestInterceptor() {
+        return new BasicAuthRequestInterceptor(username, password);
     }
 }
